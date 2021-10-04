dbc0c32dd1d63964be97e2c44152ce2dbd259227
NatanshSep2 <76418368+NatanshSep2@users.noreply.github.com>
2021-10-04 10:15:12 +0530

    Update add.c

--- add.c
+++ add.c
@@ -1,13 +1,15 @@
 #include<stdio.h>
 int main()  //main function
 {
-  int a,b;
+  int sum,a,b;
   
-   printf("Enter first number:\n");
+  printf("Enter first number:\n");
   scanf ("%d",&a);
   printf("Enter second number:\n");
   scanf ("%d",&b);
   
-  printf("Addition of two numbers = %d",a+b);
+  sum=a+b;
+  
+  printf("Addition of two numbers = %d",sum);
   return 0;
 }
